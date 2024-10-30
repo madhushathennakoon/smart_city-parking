bd0c3f608d3190429692648f1a9f79001549d1f1
COBSCCOMPY4231P-019 <cobsccompy4231p-019@NIBM-LAB04-PC05s-iMac.local>
2024-10-30 15:42:02 +0530

    initial project file push

---
+++ smart_city_parking/smart_city_parking/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  smart_city_parking
+//
+//  Created by COBSCCOMPY4231P-019 on 2024-10-30.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+            Text("MADHUSHA!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ smart_city_parking/smart_city_parking/smart_city_parkingApp.swift
@@ -0,0 +1,17 @@
+//
+//  smart_city_parkingApp.swift
+//  smart_city_parking
+//
+//  Created by COBSCCOMPY4231P-019 on 2024-10-30.
+//
+
+import SwiftUI
+
+@main
+struct smart_city_parkingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
