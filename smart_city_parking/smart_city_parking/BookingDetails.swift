fcaa4204bf78e2bb096ba18a2bd9cd70e8e6fbce
Madhusha <madhusha@Yasindu-Ascentic.local>
2024-11-09 06:56:31 +0530

    Updated UI

---
+++ smart_city_parking/smart_city_parking/BookingDetails.swift
@@ -0,0 +1,19 @@
+//
+//  BookingDetails.swift
+//  smart_city_parking
+//
+//  Created by Madhusha on 2024-11-09.
+//
+
+import SwiftUI
+
+struct BookingDetails: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Text("Booking Details")
+    }
+}
+
+#Preview {
+    BookingDetails()
+}
