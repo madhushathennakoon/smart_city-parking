f44bb8bbcfb73b53383ed7d136727410e944d7e6
COBSCCOMPY4231P-019 <cobsccompy4231p-019@NIBM-LAB04-PC03.local>
2024-11-21 19:00:03 +0530

    upadate paymentMethod and successView page

--- smart_city_parking/smart_city_parking/PaymentMethod.swift
+++ smart_city_parking/smart_city_parking/PaymentMethod.swift
@@ -9,117 +9,118 @@ import SwiftUI
 
 struct PaymentMethod: View {
     @State private var selectedPaymentMethod = "Credit/Debit Card"
-    var body: some View {
-        VStack(alignment: .leading) {
-                   
-                   HStack {
-                       Text("Payment Method")
-                           .font(.title2)
-                           .fontWeight(.bold)
-                       
-                       Spacer()
-                   }
-                   .padding(.horizontal,20)
-                   
-                   
-                   VStack(alignment: .leading, spacing: 15) {
-                       PaymentOptionRow(
-                           title: "Credit/Debit Card",
-                           isSelected: selectedPaymentMethod == "Credit/Debit Card"
-                       ) {
-                           selectedPaymentMethod = "Credit/Debit Card"
-                       }
-                       
-                       if selectedPaymentMethod == "Credit/Debit Card" {
-                           
-                           Image("CreditCard")
-                               .resizable()
-                               
-                               .frame(width:320,height: 180)
-                               .cornerRadius(10)
-                               .padding(.horizontal)
-                           
-                           // Add New Card Button
-                           Button(action: {
-                               // Add new card action
-                           }) {
-                               Text("Add New Card")
-                                   .font(.body)
-                                   .fontWeight(.bold)
-                                   .foregroundColor(.pink)
-                                   .frame(maxWidth: .infinity)
-                                   .padding()
-                                   .overlay(
-                                       RoundedRectangle(cornerRadius: 10)
-                                           .stroke(Color.pink, lineWidth: 1)
-                                   )
-                           }
-                           .padding(.horizontal)
-                       }
-                       
-                       PaymentOptionRow(
-                           title: "PayPal",
-                           isSelected: selectedPaymentMethod == "PayPal"
-                       ) {
-                           selectedPaymentMethod = "PayPal"
-                       }
-                       
-                       PaymentOptionRow(
-                           title: "Apple Pay",
-                           isSelected: selectedPaymentMethod == "Apple Pay"
-                       ) {
-                           selectedPaymentMethod = "Apple Pay"
-                       }
-                   }
-                   .padding(.horizontal)
-                   .padding(.top)
-                   
-                   Spacer()
-                   
-                   // Pay Now Button
-                   Button(action: {
-                       // Pay Now action
-                   }) {
-                       Text("Pay Now")
-                           .fontWeight(.bold)
-                           .frame(maxWidth: .infinity)
-                           .padding()
-                           .background(Color.blue)
-                           .foregroundColor(.white)
-                           .cornerRadius(10)
-                   }
-                   .padding(.horizontal)
-                   .padding(.bottom)
-                   
-                   
-               }
-               .padding(.top)
-    }
-}
-
-// Payment Option Row View
-struct PaymentOptionRow: View {
-    var title: String
-    var isSelected: Bool
-    var action: () -> Void
+    @State private var navigateToConfirmation = false 
     
     var body: some View {
-        HStack {
-            Text(title)
-                .font(.body)
-            Spacer()
-            Image(systemName: isSelected ? "checkmark.circle.fill" : "circle")
-                .foregroundColor(isSelected ? .blue : .gray)
-                .onTapGesture {
-                    action()
+        NavigationStack {
+            VStack(alignment: .leading) {
+                
+                HStack {
+                    Text("Payment Method")
+                        .font(.title2)
+                        .fontWeight(.bold)
+                    
+                    Spacer()
                 }
+                .padding(.horizontal, 20)
+                
+                VStack(alignment: .leading, spacing: 15) {
+                    PaymentOptionRow(
+                        title: "Credit/Debit Card",
+                        isSelected: selectedPaymentMethod == "Credit/Debit Card"
+                    ) {
+                        selectedPaymentMethod = "Credit/Debit Card"
+                    }
+                    
+                    if selectedPaymentMethod == "Credit/Debit Card" {
+                        
+                        Image("CreditCard")
+                            .resizable()
+                            .frame(width: 320, height: 180)
+                            .cornerRadius(10)
+                            .padding(.horizontal)
+                        
+                        Text("Add New Card")
+                            .font(.body)
+                            .fontWeight(.bold)
+                            .foregroundColor(.pink)
+                            .frame(maxWidth: .infinity)
+                            .padding()
+                            .overlay(
+                                RoundedRectangle(cornerRadius: 10)
+                                    .stroke(Color.pink, lineWidth: 1)
+                            )
+                            .padding(.horizontal)
+                    }
+                    
+                    PaymentOptionRow(
+                        title: "PayPal",
+                        isSelected: selectedPaymentMethod == "PayPal"
+                    ) {
+                        selectedPaymentMethod = "PayPal"
+                    }
+                    
+                    PaymentOptionRow(
+                        title: "Apple Pay",
+                        isSelected: selectedPaymentMethod == "Apple Pay"
+                    ) {
+                        selectedPaymentMethod = "Apple Pay"
+                    }
+                }
+                .padding(.horizontal)
+                .padding(.top)
+                
+                Spacer()
+                
+               
+                NavigationLink(destination: SuccessView(), isActive: $navigateToConfirmation) {
+                    EmptyView()
+                }
+                
+                
+                Button(action: {
+                    
+                    navigateToConfirmation = true
+                }) {
+                    Text("Pay Now")
+                        .fontWeight(.bold)
+                        .frame(maxWidth: .infinity)
+                        .padding()
+                        .background(Color.blue)
+                        .foregroundColor(.white)
+                        .cornerRadius(10)
+                }
+                .padding(.horizontal)
+                .padding(.bottom)
+                
+            }
+            .padding(.top)
         }
-        .padding()
-        .background(Color.gray.opacity(0.1))
-        .cornerRadius(10)
+    }
+    
+    // Payment Option Row View
+    struct PaymentOptionRow: View {
+        var title: String
+        var isSelected: Bool
+        var action: () -> Void
+        
+        var body: some View {
+            HStack {
+                Text(title)
+                    .font(.body)
+                Spacer()
+                Image(systemName: isSelected ? "checkmark.circle.fill" : "circle")
+                    .foregroundColor(isSelected ? .blue : .gray)
+                    .onTapGesture {
+                        action()
+                    }
+            }
+            .padding()
+            .background(Color.gray.opacity(0.1))
+            .cornerRadius(10)
+        }
     }
 }
-
 
 
 #Preview {
--- smart_city_parking/smart_city_parking/SuccessView.swift
+++ smart_city_parking/smart_city_parking/SuccessView.swift
@@ -8,64 +8,60 @@
 import SwiftUI
 
 struct SuccessView: View {
+    
+    @State private var navigateToHome = false
+    
     var body: some View {
-        VStack(spacing: 20) {
-            Spacer()
-                  ZStack {
-                      Circle()
-                          .fill(Color.pink.opacity(0.2))
-                          .frame(width: 150, height: 150)
-                      Image("Success")
-                          .resizable()
-                          .foregroundColor(.pink)
-                          .frame(width: 110, height: 110)
-                  }
-                  
-        
-                  Text("Successful!")
-                      .font(.title)
-                      .fontWeight(.bold)
-                      .foregroundColor(.black)
-           
+        NavigationStack{
+            VStack(spacing: 20) {
+                Spacer()
+                ZStack {
+                    Circle()
+                        .fill(Color.pink.opacity(0.2))
+                        .frame(width: 150, height: 150)
+                    Image("Success")
+                        .resizable()
+                        .foregroundColor(.pink)
+                        .frame(width: 110, height: 110)
+                }
+                
+                
+                Text("Successful!")
+                    .font(.title)
+                    .fontWeight(.bold)
+                    .foregroundColor(.black)
+                
+                NavigationLink(destination: MainView(), isActive: $navigateToHome) {
+                    EmptyView()
+                }
+                
+                
+                
+                
+                Button(action: {
+                    navigateToHome = true
+                }) {
+                    Text("Back to Home")
+                        .fontWeight(.bold)
+                        .frame(maxWidth: .infinity)
+                        .padding()
+                        .background(Color.blue)
+                        .foregroundColor(.white)
+                        .cornerRadius(10)
+                    
+                }
+                .padding(.horizontal,20)
+                
+                
+                Spacer()
+            }
+            .padding()
             
-                  Button(action: {
-                      // Action
-                  }) {
-                      Text("View Parking  Ticket")
-                          .fontWeight(.bold)
-                          .frame(maxWidth: .infinity)
-                          .padding()
-                          .background(Color.blue)
-                          .foregroundColor(.white)
-                          .cornerRadius(10)
-                  }
-                  .padding(.horizontal,20)
-                  
-                  
-                  Button(action: {
-                      // Action
-                  }) {
-                      Text("Back to Home")
-                          .fontWeight(.bold)
-                          .foregroundColor(.black)
-                          .frame(maxWidth: .infinity)
-                          .padding()
-                          .background(
-                              RoundedRectangle(cornerRadius: 10)
-                                  .stroke(Color.black, lineWidth: 2)
-                          )
-                          
-                  }
-                  .padding(.horizontal,20)
-                  
-                  
-            Spacer()
-              }
-              .padding()
-              
-        
-        
-        .navigationBarHidden(true)
+            
+            
+            .navigationBarHidden(true)
+            
+        }
     }
 }
 
